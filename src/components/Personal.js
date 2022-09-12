7977f4929c64bfb79e9ff050d151f26defd82365
fufako <patryko.mikiciuk@gmail.com>
2022-09-12 16:00:11 +0200

    setSate onChange in Personal inputs

--- src/components/Personal.js
+++ src/components/Personal.js
@@ -12,17 +12,61 @@ class Personal extends Component {
       description: "",
     }
   }
-
+  handleNameChange = (fname) => {
+    this.setState({ fname })
+  }
   render() {
     return (
       <div id="Personal">
         <form>
-          <input placeholder="First Name" />
-          <input placeholder="Last Name" />
-          <input placeholder="Address" />
-          <input placeholder="Phone number" />
-          <input placeholder="E-Mail" />
-          <input placeholder="Description" />
+          <input
+            placeholder="First Name"
+            onChange={(e) => {
+              this.setState({
+                fname: e.target.value,
+              })
+            }}
+          />
+          <input
+            placeholder="Last Name"
+            onChange={(e) => {
+              this.setState({
+                lname: e.target.value,
+              })
+            }}
+          />
+          <input
+            placeholder="Address"
+            onChange={(e) => {
+              this.setState({
+                address: e.target.value,
+              })
+            }}
+          />
+          <input
+            placeholder="Phone number"
+            onChange={(e) => {
+              this.setState({
+                phone: e.target.value,
+              })
+            }}
+          />
+          <input
+            placeholder="E-Mail"
+            onChange={(e) => {
+              this.setState({
+                mail: e.target.value,
+              })
+            }}
+          />
+          <input
+            placeholder="Description"
+            onChange={(e) => {
+              this.setState({
+                description: e.target.value,
+              })
+            }}
+          />
         </form>
       </div>
     )
